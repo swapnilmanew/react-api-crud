e0a686515cf7fa35cebe32e916bf22ff19c2d205
Swapnil Mane <swapnilmanew@gmail.com>
2021-05-09 07:26:38 +0000

    asdf

--- crud/src/components/Add.jsx
+++ crud/src/components/Add.jsx
@@ -5,7 +5,6 @@ const Add = () => {
         <div>
             <div className="container">
                 <h1 className="text-center fw-bold display-3 my-3">Add User</h1>
-
                 <div className="row">
                     <div className="col-12 col-md-10 m-auto">
                         <form>
